4e11df5181813ff71bfaeec937fee7b6bd50c33a
Daniel Colona <dancolm95@gmail.com>
2025-02-24 16:24:17 -0500

    primer comentario

---
+++ src/data/task.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id:0,
+    title: 'mi perimera tarea',
+    description: 'mi primera task'
+  },
+  {
+    id:1,
+    title: 'mi segunda tarea',
+    description: 'mi segunda task'
+  },
+  {
+    id:2,
+    title: 'mi tercera tarea',
+    description: 'mi tercera task'
+  }
+]
